9fb13f32c28f33b8c6f2d12318f55b212b5ed7d7
Léo <leo.dupuy21@gmail.com>
2024-12-05 13:35:45 +0100

    Première version du code où la suite de fibo est créée à chaque appui

--- main/main.c
+++ main/main.c
@@ -2,9 +2,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void blink_led(int a);
+void new_term(int *a, int b);
 int read_pin(void);
-void gen_fib_seq(int *a, int b);
 void init_led(void);
 
 void app_main(void)
@@ -12,12 +11,11 @@ void app_main(void)
     init_led();
 
     /* Tab size */
-    int N = 4;
+    int N = 6;
 
     /* Fill tab with fibonacci sequence's terms */
     int tab[N];
     int *ptr = tab;
-    gen_fib_seq(ptr, N);
 
     /* initialize counter */
     int i = 0;
@@ -28,7 +26,7 @@ void app_main(void)
         if(i < N){
             int a = read_pin();
             if( a==0 ){
-                blink_led(tab[i]);
+                new_term(ptr,i);
                 i += 1;
             }
         }
--- main/turn_on_led.s
+++ main/turn_on_led.s
@@ -1,5 +1,6 @@
 .global blink_led
-.global init_led  
+.global init_led 
+.global new_term
 
 .text
 
@@ -52,61 +53,101 @@ init_led :
 
 
 
-# a2 - adress of the register
-# t1 - value to load in the register
+# t1 - adress of the u_n value
+# t2 - adress of the u_n-1 value
+# t4 - adress of the u_n+1 value
 
 # input : 
-# a0 - number of times that the pin must turn on and off
+# a0 - pointer pointing to tab
+# a1 - position of the new element of the array
 # output :
 # nothing
-blink_led :
+new_term :
 
 	addi sp, sp, -8
-	sw ra, 4(sp)
-	sw a0, 0(sp)
+	sw ra, 0(sp)
+	sw a0, 4(sp)
+	sw a1, 8(sp)
 
-	# Check if a0 is 0. If yes add 1s delay and return
-	beq a0, x0, pause
+	# a_0 and a_1 terms
+	beq a1, x0, first_term
+	li t1, 1
+	beq a1, t1, second_term
 
-	# Loop to make the pin blink a0 times
-	loop :
-		addi a0, a0, -1
+	# get adresses of u_n+1, u_n, u_n-1
+	li t5, 4
+	mul t1, a1, t5
+	mul t2, a1, t5
+	add t4, a0, t2
+	addi t1, t1, -4
+	add t1, a0, t1
+	addi t2, t2, -8
+	add t2, a0, t2
 
-		# Register 5.4. GPIO_OUT_W1TC_REG (0x0008) : SET OUT
-		li a2, 0x60004008
-		lw t1, 0(a2)
-		ori t1, t1, 0x0100
-		sw t1, 0(a2)
+	# get and store u_n+1 value
+	lw t1, 0(t1)
+	lw t2, 0(t2)
+	add a0, t1, t2
+	sw a0, 0(t4)
+	
+	# make the led blink a0 times
+	j blink_led
 
-		# delay loop (approx 1s)
-		li t0, 27000000
-		delay_loop1:
-			addi t0, t0, -1
-			bnez t0, delay_loop1
+	first_term :
+		sw x0, 0(a0)
+		li a0, 0
+		j pause
 
-		# Register 5.4. GPIO_OUT_W1TC_REG (0x000c) : RESET OUT
-		li a2, 0x6000400c
-		lw t1, 0(a2)
-		ori t1, t1, 0x0100
-		sw t1, 0(a2)
+	second_term :
+		li t1, 1
+		sw t1, 4(a0)
+		li a0, 1
+		j blink_led
 
-		# delay loop (approx 1s)
-		li t0, 27000000
-		delay_loop2:
-			addi t0, t0, -1
-			bnez t0, delay_loop2
+	blink_led :
 
-		beq a0, x0, end
-		j loop 
+		# Loop to make the pin blink a0 times
+		loop :
+
+			addi a0, a0, -1
+
+			# Register 5.4. GPIO_OUT_W1TC_REG (0x0008) : SET OUT
+			li a2, 0x60004008
+			lw a1, 0(a2)
+			ori a1, a1, 0x0100
+			sw a1, 0(a2)
+
+			# delay loop (approx 1s)
+			li t0, 27000000
+			delay_loop1:
+				addi t0, t0, -1
+				bnez t0, delay_loop1
+
+			# Register 5.4. GPIO_OUT_W1TC_REG (0x000c) : RESET OUT
+			li a2, 0x6000400c
+			lw a1, 0(a2)
+			ori a1, a1, 0x0100
+			sw a1, 0(a2)
+
+			# delay loop (approx 1s)
+			li t0, 27000000
+			delay_loop2:
+				addi t0, t0, -1
+				bnez t0, delay_loop2
+
+			beq a0, x0, end
+			j loop
 
 	pause :
-	li t0, 27000000
-	delay_loop_pause:
-		addi t0, t0, -1 
-		bnez t0, delay_loop_pause
-	
-	end :
-	lw a0, 0(sp)
-	addi sp, sp, 8
-	jalr ra
+		li t0, 27000000
+		delay_loop_pause:
+			addi t0, t0, -1 
+			bnez t0, delay_loop_pause
+		j end
 
+	end :
+		lw ra, 0(sp)
+		lw a0, 4(sp)
+		lw a1, 8(sp)
+		addi sp, sp, 8
+		jalr ra	
